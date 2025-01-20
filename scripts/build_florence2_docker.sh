34c158fdae08d39a3ace16bbe4e4b582b18f399c
Robert Taylor2 <robert.taylor@clarifai.com>
2025-01-19 21:58:03 -0500

    added build_docker.sh to scripts

--- scripts/build_florence2_docker.sh
+++ scripts/build_florence2_docker.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker buildx build --platform linux/arm64 -f Dockerfile_r36.4.0 -t florence2:r36.4.0
+docker buildx build --platform linux/arm64 -f ../Dockerfile_r36.4.0 -t florence2:r36.4.0
